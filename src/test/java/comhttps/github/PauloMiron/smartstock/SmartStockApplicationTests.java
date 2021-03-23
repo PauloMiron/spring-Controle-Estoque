59b5b34ff00de082a8ab07eb337e0a3c561a1c49
PauloMiron <paulomiron16@gmail.com>
2021-03-23 20:42:22 -0300

    Create Project

---
+++ src/test/java/comhttps/github/PauloMiron/smartstock/SmartStockApplicationTests.java
@@ -0,0 +1,13 @@
+package comhttps.github.PauloMiron.smartstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
